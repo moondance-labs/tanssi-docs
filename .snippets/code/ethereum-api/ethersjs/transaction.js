c5bb62dfcb264a5697a8a98e7baf0120f304ae76
Albertov19 <albertoviera@gmail.com>
2023-09-04 18:16:22 +0200

    added erin feedback

--- .snippets/code/ethereum-api/ethersjs/transaction.js
+++ .snippets/code/ethereum-api/ethersjs/transaction.js
@@ -18,9 +18,9 @@ const provider = new ethers.JsonRpcProvider(providerRPC.EvmContainer.rpc, {
 
 // Define accounts and wallet
 const accountFrom = {
-  privateKey: 'YOUR_PRIVATE_KEY_HERE',
+  privateKey: 'INSERT_YOUR_PRIVATE_KEY',
 };
-const addressTo = 'ADDRESS_TO_HERE';
+const addressTo = 'INSERT_ADDRESS_TO';
 const wallet = new ethers.Wallet(accountFrom.privateKey, provider);
 
 // Create send function
