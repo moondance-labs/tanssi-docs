d186ecee9e7961f94d4be2e29b1b8dafdd02f61f
Kevin Neilson <kevin@papermoon.io>
2024-05-01 21:31:09 -0700

    merge and fix stuff

--- .snippets/code/dapp-developers/developer-toolkit/ethereum-api/precompiles/batch/web3js-batch.js
+++
@@ -1,9 +0,0 @@
-// Import the contract ABI
-const { abi } = require('./INSERT_ABI_PATH');
-
-// Find call data for the setMessage function
-const callData = web3.eth.abi.encodeFunctionCall(abi, [
-  'INSERT_INPUT_1',
-  'INSERT_INPUT_2',
-  // ...
-]);
